31cceba3702263de724a2b3ad1d0e01e52608e11
LarsEbt <larsebert0104@gmail.com>
2025-05-18 17:47:53 +0000

    Nav Component deleted, Header Component erstellt

--- WebStore/src/app/app.component.ts
+++ WebStore/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component'; // Importiere die HeaderComponent
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent], // NavigationComponent hier eintragen
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
