34503c3f6b09fb27f145ee577f459ead27210bae
LarsEbt <larsebert0104@gmail.com>
2025-05-24 10:50:45 +0000

    homepage v1

--- arqive/src/app/app.component.ts
+++ arqive/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component'; // Import hinzufügen
+import { FooterComponent } from './components/footer/footer.component'; // Import hinzufügen
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], // Header und Footer hinzufügen
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] 
 })
