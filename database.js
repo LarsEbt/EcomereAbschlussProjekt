2f08bee90871cbb063586daf4ab02b484d048cbd
ArtemisgitHu <jonassalzmann241@gmail.com>
2025-06-03 13:33:45 +0200

    database.js: Add AWS SSL profiles and other dependencies

--- database.js
+++ database.js
@@ -1,8 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '',
+  password: '#1Ecommerce',
   database: 'ECOMERCE',
-}).promise()
+});
+
+
+
+async function getProduct() {
+  const [rows] = await pool.query('SELECT * FROM products');
+  return rows;
+}
+
+const product = getProduct();
+console.log(product);
